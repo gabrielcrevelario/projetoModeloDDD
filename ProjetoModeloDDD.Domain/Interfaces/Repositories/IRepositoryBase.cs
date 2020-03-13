47f62ca29673f26d790ba697e42f83f916c7bb26
gabriel crevelario <gabriel.crevelario.k2@radixeng.com>
2020-03-13 19:31:49 -0300

    feature: first commit

---
+++ ProjetoModeloDDD.Domain/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ProjetoModeloDDD.Domain.Interfaces.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+
+    {
+        void Add(TEntity obj);
+
+        TEntity GetById(int ID);
+
+        IEnumerable<TEntity> GetAll();
+
+        void Update(TEntity obj);
+
+        void Remove(TEntity obj);
+
+        void Dispose();
+    }
+}
---
+++ ProjetoModeloDDD.MVC/Controllers/ProdutosController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using AutoMapper;
+using ProjetoModeloDDD.Application.Interface;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.MVC.ViewModels;
+
+namespace ProjetoModeloDDD.MVC.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+        private readonly IProdutoAppService _produtoApp;
+        private readonly IClienteAppService _clienteApp;
+
+        public ProdutosController(IProdutoAppService produtoApp, IClienteAppService clienteApp)
+        {
+            _produtoApp = produtoApp;
+            _clienteApp = clienteApp;
+        }
+
+        // GET: Produto/Details
+        public ActionResult Index()
+        {
+            var produtoViewModel = Mapper.Map<IEnumerable<Produto>, IEnumerable<ProdutoViewModel>>(_produtoApp.GetAll());
+
+            return View(produtoViewModel);
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClienteId = new SelectList(_clienteApp.GetAll(), "ClienteID", "Nome");
+            return View();
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProdutoViewModel produto)
+        {
+            if (ModelState.IsValid)
+            {
+                var produtoDomain = Mapper.Map<ProdutoViewModel, Produto>(produto);
+                _produtoApp.Add(produtoDomain);
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClienteId = new SelectList(_clienteApp.GetAll(), "ClienteId", "Nome", produto.ClienteId);
+            return View(produto);
+        }
+
+        // GET: Cliente/Edit
+        public ActionResult Edit(int id)
+        {
+            var produto = _produtoApp.GetById(id);
+            var produtoViewModel = Mapper.Map<Produto, ProdutoViewModel>(produto);
+
+            ViewBag.ClienteId = new SelectList(_clienteApp.GetAll(), "ClienteId", "Nome", produtoViewModel.ClienteId);
+            
+            return View(produtoViewModel);
+        }
+
+        // POST: Produto/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProdutoViewModel produto)
+        {
+            if (ModelState.IsValid)
+            {
+                var produtoDomain = Mapper.Map<ProdutoViewModel, Produto>(produto);
+                    _produtoApp.Add(produtoDomain);
+                
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClienteId = new SelectList(_clienteApp.GetAll(), "ClienteId", "Nome", produto.ClienteId);
+            return View(produto);
+        }
+
+
+        // GET: Produto/Delete
+        public ActionResult Delete(int id)
+        {
+            var produto = _produtoApp.GetById(id);
+            var produtoViewModel = Mapper.Map<Produto, ProdutoViewModel>(produto);
+
+            return View(produtoViewModel);
+        }
+
+        // POST: Cliente/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var produto = _produtoApp.GetById(id);
+            _produtoApp.Remover(produto);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
